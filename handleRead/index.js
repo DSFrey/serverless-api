3c800ae7e101af55785be300a5cc2762196ebeac
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-23 01:39:22 +0000

    Complete handleRead

--- handleRead/index.js
+++ handleRead/index.js
@@ -3,6 +3,30 @@ const dynamoose = require('dynamoose');
 const schema = new dynamoose.Schema({
   id: String,
   name: String,
-  age: String,
+  age: Number,
 });
 
+const people = dynamoose.model('people-lab-18', schema);
+
+exports.handler = async (event) => {
+  try {
+    const id = event.pathParameters?.id;
+    let data;
+    if (id) {
+      let search = await people.query('id').eq(id).exec();
+      data = search[0]
+    } else {
+      data = await people.scan().exec();
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message)
+    };
+  }
+}
